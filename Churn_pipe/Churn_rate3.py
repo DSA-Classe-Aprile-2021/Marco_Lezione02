25ad7b5ad9bf2c90fe7a827a7a074c5e67492f24
Marco d'Onofrio <donny.mainster@gmail.com>
2021-06-01 16:53:22 +0200

    Error solved

--- Churn_pipe/Churn_rate3.py
+++ Churn_pipe/Churn_rate3.py
@@ -10,6 +10,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression 
 from penguin import Processing
+from sklearn.pipeline import make_pipeline
+from sklearn.model_selection import GridSearchCV
 
 
 # In[2]:
@@ -21,17 +23,26 @@ df = Processing('Churn_rate_train.csv')
 # In[3]:
 
 
-X, y = df.Preprocessing(colname = ['churn','international_plan', 'voice_mail_plan'], value = 'yes', target = 'churn')
+X, y = df.Preprocessing(colname = ['international_plan', 'voice_mail_plan', 'churn'], value = 'yes', target = 'churn')
 
 
-# In[ ]:
+# In[4]:
 
 
+pipe = make_pipeline(StandardScaler(), 
+                     LogisticRegression())
 
 
-
-# In[ ]:
-
+# In[5]:
 
 
+p_grid = {'logisticregression__C': [1, 10, 100],
+    'logisticregression__tol': [.01, .1]
+    }
+
+gs = GridSearchCV(pipe, param_grid = p_grid, cv = 5, scoring = "roc_auc")
+
+gs.fit(X, y)
+print(gs.best_estimator_)
+print(gs.score(X, y))
 
